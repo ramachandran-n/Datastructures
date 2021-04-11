55bc80006a29d296ce644d4a50465187e09c7a38
Ramachandran Narayanan <ramachandran.narayanan@live.com>
2021-04-12 00:09:58 +0530

    Bubble sort

--- Consumer/DataStructureConsumer.cs
+++ Consumer/DataStructureConsumer.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DataStructure;
+using DataStructure.Sort;
 
 namespace DataStructure.Search
 {
@@ -12,21 +13,23 @@ namespace DataStructure.Search
         static void Main(string[] args)
         {
             List<int> inputList = new List<int>();
+            inputList.Add(33);
+            inputList.Add(100);
             inputList.Add(10);
             inputList.Add(14);
+            inputList.Add(35);
+            inputList.Add(42);
+            inputList.Add(44);
             inputList.Add(19);
             inputList.Add(26);
             inputList.Add(27);
             inputList.Add(31);
-            inputList.Add(33);
-            inputList.Add(35);
-            inputList.Add(42);
-            inputList.Add(44);
+            inputList.Add(-1);
 
             List<double> doubleInputList = new List<double>();
-            doubleInputList.Add(10.0);
-            doubleInputList.Add(14.0);
-            doubleInputList.Add(19.0);
+            doubleInputList.Add(-10.0);
+            doubleInputList.Add(-14.0);
+            doubleInputList.Add(-19.0);
             doubleInputList.Add(26.0);
             doubleInputList.Add(27.0);
             doubleInputList.Add(31.0);
@@ -34,7 +37,11 @@ namespace DataStructure.Search
             doubleInputList.Add(35.0);
             doubleInputList.Add(42.0);
             doubleInputList.Add(44.0);
-            
+            doubleInputList.Add(10.0);
+            doubleInputList.Add(14.0);
+            doubleInputList.Add(19.0);
+            doubleInputList.Add(26.0);
+
 
             List<string> stringInputList = new List<string>();
             stringInputList.Add("Ramachandran");
@@ -47,7 +54,12 @@ namespace DataStructure.Search
             //Console.WriteLine(Searching.LinearSearch(stringInputList, "Ram"));
             //Console.WriteLine(Searching.BinarySearch(inputList, 19));
             //Console.WriteLine(Searching.BinarySearch(doubleInputList, 19.0));
-            Console.WriteLine(Searching.BinarySearch(stringInputList, "Ramachandran"));
+            //Console.WriteLine(Searching.BinarySearch(stringInputList, "Ramachandran"));
+            var outputArray = Sorting.BubbleSort(doubleInputList);
+            foreach (var element in outputArray)
+            {
+                Console.WriteLine(element);
+            }
 
         }
     }
---
+++ DataStructure/Sorting.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Sort
+{
+    public static class Sorting
+    {
+        public static List<T> BubbleSort<T>(List<T> inputArray)
+        {
+            //Get the count of the inputArray
+            int arrayLengh = inputArray.Count();
+            //Set the sorted flag
+            bool sorted;
+            //Set the temp variable
+            T temp;
+            //Outer loop
+            for (int i = 0; i < arrayLengh; i++)
+            {
+                //Set the value of the sorted flag
+                sorted = false;
+                //Inner loop
+                for (int j = 0; j < arrayLengh-1; j++)
+                {
+                    if(Comparer<T>.Default.Compare(inputArray[j], inputArray[j + 1]) == 1)
+                    {
+                        temp = inputArray[j];
+                        inputArray[j] = inputArray[j + 1];
+                        inputArray[j + 1] = temp;
+                        //inputArray[j] = inputArray[j]  inputArray[j+1];
+                        //inputArray[j + 1] = inputArray[j] - inputArray[j + 1];
+                        //inputArray[j] = inputArray[j] - inputArray[j + 1];
+                        sorted = true;
+                    }
+                }
+
+                //Check for the sorted flag - if false all elements are sorted
+                if(sorted==false)
+                    //If sorted is false break the loop
+                    break;
+            }
+            return inputArray;
+        }
+    }
+}
